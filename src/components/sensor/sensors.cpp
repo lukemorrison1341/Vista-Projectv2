9bf33e599d58875e4d63e9cbc1aa25cc1da3f60b
lukemorrison1341 <lukemorrison1341@gmail.com>
2025-04-12 23:29:56 -0400

    Test2

--- src/components/sensor/sensors.cpp
+++ src/components/sensor/sensors.cpp
@@ -13,7 +13,7 @@ boolean get_pir(){
     return digitalRead(PIR_INPUT) == HIGH ? true : false;
 }
 void config_pir(){
-    pinMode(PIR_INPUT,INPUT);
+    //pinMode(PIR_INPUT,INPUT);
     Serial.println("PIR Sensor configured");
 }
 
